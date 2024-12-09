0be1de07ed17780cfa981e1598c0bba5a2654193
Natalia Shaikhutdinova <nat12031417@gmail.com>
2024-12-09 13:15:28 +0300

    add gradle dependencies + application.properties

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,2 @@
-rootProject.name = "cashbackBot"
+rootProject.name = "bonuscheckbot"
 
---
+++ src/main/java/org/twominds/bonuscheck/bonusCheckApplication.java
@@ -0,0 +1,11 @@
+package org.twominds.bonuscheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class bonusCheckApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(bonusCheckApplication.class, args);
+    }
+}
